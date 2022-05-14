74ee3ac9fde07bf62bfccdfff483c69eb0e085d1
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-05-14 14:13:00 +0900

    [구현] rtk 설정

---
+++ src/slice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export type InitialStateProp = {
+    isLogedIn:boolean,
+    value: number
+}
+
+export const { actions, reducer }  = createSlice({
+  name: 'app',
+  initialState: {
+    isLogedIn: false,
+  },
+  reducers: {
+    setLogedIn: (state,{payload}) => {      
+      if(payload){
+        const tokens = {
+          address1:'accessToken'
+        }
+        localStorage.setItem('ajs_user_id','address1')  
+        localStorage.setItem('tokens',JSON.stringify(tokens))
+      }else{    
+        const tokens = JSON.parse(localStorage.getItem('tokens')||"{}")
+        if(tokens){
+          tokens[localStorage.getItem('ajs_user_id')||""] = ""
+        }
+        localStorage.setItem('ajs_user_id','')  
+        localStorage.setItem('tokens',JSON.stringify(tokens))
+      }
+      return {
+        ...state,
+      isLogedIn :payload
+      }
+    }
+  }
+})
+
+export const {
+  setLogedIn,
+} = actions
